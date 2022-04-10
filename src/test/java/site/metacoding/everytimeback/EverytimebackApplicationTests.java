5ed174a2cc7a1db3b8c763bbb42c36ef21a1f9f9
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-11 00:16:40 +0900

    [정재원] Initial commit

---
+++ src/test/java/site/metacoding/everytimeback/EverytimebackApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.everytimeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverytimebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
