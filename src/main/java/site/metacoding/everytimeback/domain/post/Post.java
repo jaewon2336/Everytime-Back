bb921173b989e169ab27b18e64b58a1ff3d3546d
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-12 12:23:04 +0900

    [정재원] 에브리타임 모델링 완료

--- src/main/java/site/metacoding/everytimeback/domain/post/Post.java
+++ src/main/java/site/metacoding/everytimeback/domain/post/Post.java
@@ -1,5 +1,63 @@
 package site.metacoding.everytimeback.domain.post;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import site.metacoding.everytimeback.domain.user.User;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 60, nullable = false)
+    private String title;
+
+    @Lob // 4GB
+    @Column(nullable = true)
+    private String content;
+
+    @Column(length = 200, nullable = false)
+    private String thumnail;
+
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private Integer boardNo;
+
+    private Integer likeCount;
+
+    private boolean anonyCheck;
+
+    @Column(length = 200)
+    private String tag;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime updateDate;
 }
